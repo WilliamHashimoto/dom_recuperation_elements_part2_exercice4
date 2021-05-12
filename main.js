38d2243a14804820e1a2f543016c6f3020bccbd5
WilliamHashimoto <79861381+WilliamHashimoto@users.noreply.github.com>
2021-05-12 11:37:00 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//1
+let exo1 = document.getElementsByClassName("redPurple");
+console.log(exo1);
+
+//2
+let exo2 = document.querySelectorAll(".redPurple");
+console.log(exo2);
+
+//3
+let exo3 = document.querySelectorAll("h1.redPurple");
+console.log(exo3);
+
+//4
+let exo4 = document.querySelectorAll("li, p, span");
+console.log(exo4);
+
+//5
+let exo5 = document.querySelectorAll("li.important, p");
+console.log(exo5);
+
+//6
+let exo6 = document.querySelectorAll("h1.redPurple, span.redPurple");
+console.log(exo6);
